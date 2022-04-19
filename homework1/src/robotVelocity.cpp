9a8fc1f5e82e431e00c1d2375b24d93deb3abb6a
ClaoGali <45966392+ClaoGali@users.noreply.github.com>
2022-04-19 17:25:32 +0200

    Add files via upload

--- homework1/src/robotVelocity.cpp
+++ homework1/src/robotVelocity.cpp
@@ -1,4 +1,5 @@
 #define _USE_MATH_DEFINES
+#include <iostream>
 #include <cmath>
 #include <sstream>
 #include <boost/shared_ptr.hpp>
@@ -21,30 +22,42 @@ class robot_velocity{
         float wheelTickPrevious[4];
         float wheelTick[4];
         float wheelRpmFromTick[4];
-
+        int count =223;
+        double vx;
+        double vy;
+        double wz;
     public:
 
          robot_velocity(){
             sub = n.subscribe("/wheel_states", 1, &robot_velocity::savingPosition, this);
+            for(int i=0; i<4; i++){
+                wheelTickPrevious[i] = 0;
+                wheelTick[i] = 0;
+                wheelRpmFromTick[i] = 0;
+            }
+            vx = 0;
+            vy = 0;
+            wz = 0;
         }
 
         void savingPosition(const sensor_msgs::JointState::ConstPtr& msg){
             double delta_t;
            
             delta_t = (msg->header.stamp.operator-(this->t_previous)).toSec();
+            printf("%d Calculated rad/min: ", count);
             for(int i=0; i<4; i++){
                 wheelTick[i] = msg->position[i];
                 if(wheelTickPrevious[0] != 0){
                     wheelRpmFromTick[i] = (wheelTick[i]-wheelTickPrevious[i])*2*60*3.14/(encoderResolution*delta_t);
                 }
-                
-                ROS_INFO("I heard : [%f]", wheelRpmFromTick[i]);
+                vx = 1/2*radius*(wheelRpmFromTick[0]+wheelRpmFromTick[1]);
+                vy = 1/2*radius*(wheelRpmFromTick[1]-wheelRpmFromTick[2]);
+                wz = radius*(wheelRpmFromTick[2]-wheelRpmFromTick[0])/(2*(distanceFromCenterX+distanceFromCenterY));
+                printf("%f  ", wheelRpmFromTick[i]);
+                //ROS_INFO("I heard : [%f]", wheelRpmFromTick[i]);
             }
-            /*
-            ROS_INFO("t previous : [%f]", t_previous);
-            ROS_INFO("t : [%f]", msg->header.stamp);
-            ROS_INFO("Delta t : [%f]", delta_t);
-            */
+            printf("\n");
+            count ++;
             this->t_previous = msg->header.stamp;
             for(int j=0; j<4; j++){
                 wheelTickPrevious[j] = wheelTick[j];           }
@@ -58,3 +71,9 @@ int main(int argc, char **argv){
     ros::spin();
     return 0;
 }
+
+    /*
+    ROS_INFO("t previous : [%f]", t_previous);
+    ROS_INFO("t : [%f]", msg->header.stamp);
+    ROS_INFO("Delta t : [%f]", delta_t);
+    */
